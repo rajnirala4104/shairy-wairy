b87001d8805fb2ea69990fe8f0f51aeb77808cea
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2025-10-29 13:02:10 +0530

    chore: setup the project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { Fragment } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <Fragment>
+      <div className="w-full h-screen border border-red-500 flex justify-center items-center">
+        <span className="text-2xl">
+          "Shairy-Wairy"
+        </span>
+      </div>
+    </Fragment>
+  )
+}
+
+export default App
